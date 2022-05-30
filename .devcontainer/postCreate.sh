fff85ba34eb5511668858deb47d19fe84fb0bb84
Alexandr <53787217+qwerty541@users.noreply.github.com>
2022-05-30 03:16:56 +0000

    dev

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,5 @@ cd ./opencart/
 git checkout 8b4144edc3d54bfabea1295f936f7962e3a81d59
 mv ./upload/config-dist.php ./upload/config.php
 mv ./upload/admin/config-dist.php ./upload/admin/config.php
+rm -rf ./upload/system/storage/vendor/
+composer install
