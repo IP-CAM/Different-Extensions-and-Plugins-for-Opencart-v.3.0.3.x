1773c72070a0e81c46991d664de93cb1a7101e97
Alexandr <53787217+qwerty541@users.noreply.github.com>
2022-05-31 01:32:44 +0000

    dev

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,15 +1,17 @@
-cd /workspaces/opencart-plugins/
+WORKSPACE="/workspaces/opencart-plugins/"
+cd $WORKSPACE
+
+# Clone opencart and checkout to 3.0.3.8
+OPENCART="${WORKSPACE}opencart"
+rm -rf $OPENCART
 git clone https://github.com/opencart/opencart.git
-cd ./opencart/
+cd $OPENCART
 git checkout 8b4144edc3d54bfabea1295f936f7962e3a81d59
+
+# Prepare config files
 mv ./upload/config-dist.php ./upload/config.php
 mv ./upload/admin/config-dist.php ./upload/admin/config.php
-rm -rf ./upload/system/storage/vendor/
-mkdir /workspaces/opencart-plugins/composer-temp/
-cd /workspaces/opencart-plugins/composer-temp/
-cp ../opencart/composer.json ./composer.json
-cp ../opencart/composer.lock ./composer.lock
+
+# Rebuild composer dependencies
+rm -rf ./upload/system/storage/vendor
 composer install
-cd /workspaces/opencart-plugins/
-mv ./composer-temp/system/storage/vendor/ ./opencart/upload/system/storage/vendor/
-rm -rf /workspaces/opencart-plugins/composer-temp/
